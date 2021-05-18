9f3bbf38706b25f0e4b8b977ad353ec16c80dfe7
passerby-b <8163312+passerby-b@user.noreply.gitee.com>
2021-05-18 11:57:29 +0800

    update test/jd_price.js.

--- test/jd_price.js
+++ test/jd_price.js
@@ -1,55 +1,61 @@
 
-
-
-
 const $ = new API("jddj_fruit");
-let reqbody = 'area=17_1381_3079_50776&body=%7B%22source_type%22%3A%22search%22%2C%22skuId%22%3A%2210026665149685%22%2C%22darkModelEnum%22%3A3%2C%22gcLng%22%3A%22114.31944%22%2C%22ocrFlag%22%3Afalse%2C%22brand%22%3A%22iPhone%22%2C%22fullAddress%22%3A%22IPQa4MLDO91tqXQP4Vb25d2L3BXms0LbdlS0znnf2mAKL9tiNGpLZFfH9iIajignGhkW1Oy3KZJYVUtZ%2BO9B1iRQG9k62hXAVaqh9cGQU0Q%3D%22%2C%22isDesCbc%22%3Atrue%2C%22latitude%22%3A%2230.471916%22%2C%22plusClickCount%22%3A0%2C%22eventId%22%3A%22Searchlist_Productid%22%2C%22abTest800%22%3Atrue%2C%22gcLat%22%3A%2230.51296%22%2C%22avoidLive%22%3Afalse%2C%22uAddrId%22%3A%22397459499%22%2C%22longitude%22%3A%22114.319396%22%2C%22provinceId%22%3A%2217%22%2C%22personas%22%3A%22personas%22%2C%22cityId%22%3A%221381%22%2C%22townId%22%3A%2253770%22%2C%22plusLandedFatigue%22%3A0%2C%22districtId%22%3A%2250718%22%7D&build=167668&client=apple&clientVersion=9.5.4&d_brand=apple&d_model=iPhone11%2C6&eid=eidIced481213ds6JDJSO9UVRTeIiYarBrw9tyQyU7TJ7ASAv8IvIuGBRXJqZtQabKIIEWNsu6iVM%2BJfXJO%2BuFBPr5SZNYoL1uBcaTQQKJXBBOoI2TA4&isBackground=N&joycious=54&lang=zh_CN&networkType=wifi&networklibtype=JDNetworkBaseAF&openudid=311fc185ed97a0392e35657dfe2a321664170965&osVersion=14.1&partner=apple&rfs=0000&scope=10&screen=1242%2A2688&sign=d09476d00b52f4aab9638870019ffda5&st=1621304931737&sv=101&uemps=0-0&uts=0f31TVRjBSsqndu4/jgUPz6uymy50MQJc17HJwR/QxfZBXypHHkUHofYpPnkVnPSWhexFlhweNmTevr%2B8QeylBStFdHhycMIJyjAMvNu4tKBf5I/0jdz8a9ChoDEajx6b3H0b8Owoh8oKSUczhVCYK2tTaK9YKFHBDpaWaLdhTyKA4RI3fjtSzgDrmlh2Rph3c8jFYY5fpRjHC0z6SjLDw%3D%3D&uuid=hjudwgohxzVu96krv/T6Hg%3D%3D&wifiBssid=3d6e696f46a52bb20ed0de232f820824';//$request.body;
-let jsonparm = {};
-let params = decodeURIComponent(reqbody).split('&');
-params.forEach(item => {
-    if (item.indexOf('=') > -1) jsonparm[item.split('=')[0].trim()] = item.split('=')[1].trim();
-});
-let skuId = JSON.parse(jsonparm.body + '"}').skuId;
+!(async () => {
 
-let price = '';
-$.http.get({
-    url: 'http://p.zwjhl.com/price.aspx?url=https%3A%2F%2Fitem.jd.com%2F' + skuId + '.html&event=searchPrice',
-    headers: {
-        'Accept': 'text / html, application/ xhtml + xml, application/ xml; q = 0.9, image / avif, image / webp, image / apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-        'Cookie': 'ASP.NET_SessionId=nwq52zavmpj0ihtcxi42qprj; lsjgcx_userdev=%7b%22FirstDate%22%3a%222021-05-19,+09%3a19%3a29%22%2c%22FirstLoginDate%22%3a%22%22%2c%22LastLoginDate%22%3a%22%22%2c%22DevNum%22%3a%226d3b1021cf1f49cca4d569db94ff6378%22%7d',
-        'Host': 'p.zwjhl.com',
-        'Proxy-Connection': 'keep-alive',
-        'Referer': 'http://p.zwjhl.com/price.aspx?url=https%3A%2F%2Fitem.jd.com%2F' + skuId + '.html&event=searchPrice',
-        'Upgrade-Insecure-Requests': 1,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
-    }
-}).then(response => {
-    //console.log(response.body);
-    let str = response.body;
-    let s = str.indexOf('flotChart.chartNow(\'') + 20;
-    let e = str.indexOf(',\'https://item.jd.com/') - 1;
-    let data = JSON.parse('[' + str.substring(s, e) + ']');
+    let reqbody = 'area=17_1381_3079_50776&body=%7B%22source_type%22%3A%22search%22%2C%22skuId%22%3A%2210026665149685%22%2C%22darkModelEnum%22%3A3%2C%22gcLng%22%3A%22114.31944%22%2C%22ocrFlag%22%3Afalse%2C%22brand%22%3A%22iPhone%22%2C%22fullAddress%22%3A%22IPQa4MLDO91tqXQP4Vb25d2L3BXms0LbdlS0znnf2mAKL9tiNGpLZFfH9iIajignGhkW1Oy3KZJYVUtZ%2BO9B1iRQG9k62hXAVaqh9cGQU0Q%3D%22%2C%22isDesCbc%22%3Atrue%2C%22latitude%22%3A%2230.471916%22%2C%22plusClickCount%22%3A0%2C%22eventId%22%3A%22Searchlist_Productid%22%2C%22abTest800%22%3Atrue%2C%22gcLat%22%3A%2230.51296%22%2C%22avoidLive%22%3Afalse%2C%22uAddrId%22%3A%22397459499%22%2C%22longitude%22%3A%22114.319396%22%2C%22provinceId%22%3A%2217%22%2C%22personas%22%3A%22personas%22%2C%22cityId%22%3A%221381%22%2C%22townId%22%3A%2253770%22%2C%22plusLandedFatigue%22%3A0%2C%22districtId%22%3A%2250718%22%7D&build=167668&client=apple&clientVersion=9.5.4&d_brand=apple&d_model=iPhone11%2C6&eid=eidIced481213ds6JDJSO9UVRTeIiYarBrw9tyQyU7TJ7ASAv8IvIuGBRXJqZtQabKIIEWNsu6iVM%2BJfXJO%2BuFBPr5SZNYoL1uBcaTQQKJXBBOoI2TA4&isBackground=N&joycious=54&lang=zh_CN&networkType=wifi&networklibtype=JDNetworkBaseAF&openudid=311fc185ed97a0392e35657dfe2a321664170965&osVersion=14.1&partner=apple&rfs=0000&scope=10&screen=1242%2A2688&sign=d09476d00b52f4aab9638870019ffda5&st=1621304931737&sv=101&uemps=0-0&uts=0f31TVRjBSsqndu4/jgUPz6uymy50MQJc17HJwR/QxfZBXypHHkUHofYpPnkVnPSWhexFlhweNmTevr%2B8QeylBStFdHhycMIJyjAMvNu4tKBf5I/0jdz8a9ChoDEajx6b3H0b8Owoh8oKSUczhVCYK2tTaK9YKFHBDpaWaLdhTyKA4RI3fjtSzgDrmlh2Rph3c8jFYY5fpRjHC0z6SjLDw%3D%3D&uuid=hjudwgohxzVu96krv/T6Hg%3D%3D&wifiBssid=3d6e696f46a52bb20ed0de232f820824';
+    //$request.body;
+    console.log($request.body);
+    let jsonparm = {};
+    let params = decodeURIComponent(reqbody).split('&');
+    params.forEach(item => {
+        if (item.indexOf('=') > -1) jsonparm[item.split('=')[0].trim()] = item.split('=')[1].trim();
+    });
+    let skuId = JSON.parse(jsonparm.body + '"}').skuId;
 
-    let max = 0, min = 999999999;
-    for (let i = 0; i < data.length; i++) {
-        let date = new Date(parseInt(data[i][0])).toLocaleString().replace(/:\d{1,2}$/, ' ');
-        //console.log(date, data[i][1]);
-        if (max < data[i][1]) {
-            max = data[i][1];
+    let price = '';
+    $.http.get({
+        url: 'http://p.zwjhl.com/price.aspx?url=https%3A%2F%2Fitem.jd.com%2F' + skuId + '.html&event=searchPrice',
+        headers: {
+            'Accept': 'text / html, application/ xhtml + xml, application/ xml; q = 0.9, image / avif, image / webp, image / apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
+            'Cookie': 'ASP.NET_SessionId=nwq52zavmpj0ihtcxi42qprj; lsjgcx_userdev=%7b%22FirstDate%22%3a%222021-05-19,+09%3a19%3a29%22%2c%22FirstLoginDate%22%3a%22%22%2c%22LastLoginDate%22%3a%22%22%2c%22DevNum%22%3a%226d3b1021cf1f49cca4d569db94ff6378%22%7d',
+            'Host': 'p.zwjhl.com',
+            'Proxy-Connection': 'keep-alive',
+            'Referer': 'http://p.zwjhl.com/price.aspx?url=https%3A%2F%2Fitem.jd.com%2F' + skuId + '.html&event=searchPrice',
+            'Upgrade-Insecure-Requests': 1,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
         }
-        if (min > data[i][1]) {
-            min = data[i][1];
+    }).then(response => {
+        //console.log(response.body);
+        let str = response.body;
+        let s = str.indexOf('flotChart.chartNow(\'') + 20;
+        let e = str.indexOf(',\'https://item.jd.com/') - 1;
+        let data = JSON.parse('[' + str.substring(s, e) + ']');
+
+        let max = 0, min = 999999999;
+        for (let i = 0; i < data.length; i++) {
+            let date = new Date(parseInt(data[i][0])).toLocaleString().replace(/:\d{1,2}$/, ' ');
+            //console.log(date, data[i][1]);
+            if (max < data[i][1]) {
+                max = data[i][1];
+            }
+            if (min > data[i][1]) {
+                min = data[i][1];
+            }
         }
-    }
-    console.log('近三十天最低价:' + min + ',最高价:' + max);
-    price = '近三十天最低价:' + min + ',最高价:' + max;
+        console.log('近三十天最低价:' + min + ',最高价:' + max);
+        price = '近三十天最低价:' + min + ',最高价:' + max;
+    });
+
+    let resbody = $response.body;
+    $done({ body: JSON.stringify(resbody) });
+
+})().catch(async (e) => {
+    console.log('', '❌失败! 原因:' + e + '!', '');
+}).finally(() => {
+    $.done();
 });
-let resbody = $response.body;
-console.log(resbody);
-let resdata = JSON.parse(resbody);
-resdata.floors[13].data.ad.adword = price;
-$done({ body: JSON.stringify(resdata) });
+
 
 /*********************************** API *************************************/
 function ENV() { const e = "undefined" != typeof $task, t = "undefined" != typeof $loon, s = "undefined" != typeof $httpClient && !t, i = "function" == typeof require && "undefined" != typeof $jsbox; return { isQX: e, isLoon: t, isSurge: s, isNode: "function" == typeof require && !i, isJSBox: i, isRequest: "undefined" != typeof $request, isScriptable: "undefined" != typeof importModule } } function HTTP(e = { baseURL: "" }) { const { isQX: t, isLoon: s, isSurge: i, isScriptable: n, isNode: o } = ENV(), r = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/; const u = {}; return ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"].forEach(l => u[l.toLowerCase()] = (u => (function (u, l) { l = "string" == typeof l ? { url: l } : l; const h = e.baseURL; h && !r.test(l.url || "") && (l.url = h ? h + l.url : l.url); const a = (l = { ...e, ...l }).timeout, c = { onRequest: () => { }, onResponse: e => e, onTimeout: () => { }, ...l.events }; let f, d; if (c.onRequest(u, l), t) f = $task.fetch({ method: u, ...l }); else if (s || i || o) f = new Promise((e, t) => { (o ? require("request") : $httpClient)[u.toLowerCase()](l, (s, i, n) => { s ? t(s) : e({ statusCode: i.status || i.statusCode, headers: i.headers, body: n }) }) }); else if (n) { const e = new Request(l.url); e.method = u, e.headers = l.headers, e.body = l.body, f = new Promise((t, s) => { e.loadString().then(s => { t({ statusCode: e.response.statusCode, headers: e.response.headers, body: s }) }).catch(e => s(e)) }) } const p = a ? new Promise((e, t) => { d = setTimeout(() => (c.onTimeout(), t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)), a) }) : null; return (p ? Promise.race([p, f]).then(e => (clearTimeout(d), e)) : f).then(e => c.onResponse(e)) })(l, u))), u } function API(e = "untitled", t = !1) { const { isQX: s, isLoon: i, isSurge: n, isNode: o, isJSBox: r, isScriptable: u } = ENV(); return new class { constructor(e, t) { this.name = e, this.debug = t, this.http = HTTP(), this.env = ENV(), this.node = (() => { if (o) { return { fs: require("fs") } } return null })(), this.initCache(); Promise.prototype.delay = function (e) { return this.then(function (t) { return ((e, t) => new Promise(function (s) { setTimeout(s.bind(null, t), e) }))(e, t) }) } } initCache() { if (s && (this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}")), (i || n) && (this.cache = JSON.parse($persistentStore.read(this.name) || "{}")), o) { let e = "root.json"; this.node.fs.existsSync(e) || this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.root = {}, e = `${this.name}.json`, this.node.fs.existsSync(e) ? this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)) : (this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.cache = {}) } } persistCache() { const e = JSON.stringify(this.cache, null, 2); s && $prefs.setValueForKey(e, this.name), (i || n) && $persistentStore.write(e, this.name), o && (this.node.fs.writeFileSync(`${this.name}.json`, e, { flag: "w" }, e => console.log(e)), this.node.fs.writeFileSync("root.json", JSON.stringify(this.root, null, 2), { flag: "w" }, e => console.log(e))) } write(e, t) { if (this.log(`SET ${t}`), -1 !== t.indexOf("#")) { if (t = t.substr(1), n || i) return $persistentStore.write(e, t); if (s) return $prefs.setValueForKey(e, t); o && (this.root[t] = e) } else this.cache[t] = e; this.persistCache() } read(e) { return this.log(`READ ${e}`), -1 === e.indexOf("#") ? this.cache[e] : (e = e.substr(1), n || i ? $persistentStore.read(e) : s ? $prefs.valueForKey(e) : o ? this.root[e] : void 0) } delete(e) { if (this.log(`DELETE ${e}`), -1 !== e.indexOf("#")) { if (e = e.substr(1), n || i) return $persistentStore.write(null, e); if (s) return $prefs.removeValueForKey(e); o && delete this.root[e] } else delete this.cache[e]; this.persistCache() } notify(e, t = "", l = "", h = {}) { const a = h["open-url"], c = h["media-url"]; if (s && $notify(e, t, l, h), n && $notification.post(e, t, l + `${c ? "\n多媒体:" + c : ""}`, { url: a }), i) { let s = {}; a && (s.openUrl = a), c && (s.mediaUrl = c), "{}" === JSON.stringify(s) ? $notification.post(e, t, l) : $notification.post(e, t, l, s) } if (o || u) { const s = l + (a ? `\n点击跳转: ${a}` : "") + (c ? `\n多媒体: ${c}` : ""); if (r) { require("push").schedule({ title: e, body: (t ? t + "\n" : "") + s }) } else console.log(`${e}\n${t}\n${s}\n\n`) } } log(e) { this.debug && console.log(`[${this.name}] LOG: ${this.stringify(e)}`) } info(e) { console.log(`[${this.name}] INFO: ${this.stringify(e)}`) } error(e) { console.log(`[${this.name}] ERROR: ${this.stringify(e)}`) } wait(e) { return new Promise(t => setTimeout(t, e)) } done(e = {}) { console.log('done!'); s || i || n ? $done(e) : o && !r && "undefined" != typeof $context && ($context.headers = e.headers, $context.statusCode = e.statusCode, $context.body = e.body) } stringify(e) { if ("string" == typeof e || e instanceof String) return e; try { return JSON.stringify(e, null, 2) } catch (e) { return "[object Object]" } } }(e, t) }
