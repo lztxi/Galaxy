912ddaaa04e7fae8c43087cf2f1f40f9a6d8b141
passerby-b <8163312+passerby-b@user.noreply.gitee.com>
2021-06-04 15:06:30 +0800

    update test/moji_loc.js.

--- test/moji_loc.js
+++ test/moji_loc.js
@@ -4,7 +4,8 @@ const $ = new API("moji_loc");
     let body = $request.body;
     console.log(body);
     let data = JSON.parse(body);
-    $.notify(data.params.city[0].lon, data.params.city[0].lat, '');
+    //$.notify(data.params.city[0].lon, data.params.city[0].lat, '');
+    $.notify('11', '22', '');
 
 })().catch(async (e) => {
     console.log('', '❌失败! 原因:' + e + '!', '');
