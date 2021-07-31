c1672dd278ca2dfb9f18d3be56ba7fb920eb05db
passerby-b <8163312+passerby-b@user.noreply.gitee.com>
2021-07-31 07:51:06 +0000

    update test/jddj_fruit.js.

--- test/jddj_fruit.js
+++ test/jddj_fruit.js
@@ -614,6 +614,7 @@ function _uuid() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     }
+    console.log(s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4());
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
